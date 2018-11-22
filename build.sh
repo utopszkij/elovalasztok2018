0c4c738d2b076e96f9ecc7a2d90e5b40f79e1c32
utopszkij <tibor.fogler@gmail.com>
2018-11-22 10:22:31 +0100

    try travis

--- build.sh
+++ build.sh
@@ -1,16 +1,19 @@
-cd test;
+#!/bin/sh
+# rin php unittest and run sonar-scanner
+cd test
 phpunit --whitelist . \
 --coverage-clover ../unittest.xml \
 --log-junit ../unittestlog.xml \
-.;
-
+.
 retVal=$?
-if [ $retVal -ne 0 ]; then
-  cd ..;
+if [ $retVal -eq 0 ]; then
+  cd ..
   sonar-scanner \
   -Dsonar.projectKey=utopszkij_elovalasztok2018 \
   -Dsonar.organization=utopszkij-github \
   -Dsonar.sources=component/site \
   -Dsonar.host.url=https://sonarcloud.io \
   -Dsonar.login=47b18c5eb028e4cd9651f98af0eed7f6f070d419;  
+  retval=$?
 fi
+exit $retval
